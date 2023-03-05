e80cc60805d1b22360db1ef99325c80e35d73b1a
RajiGaikwad <125719742+RajiGaikwad@users.noreply.github.com>
2023-03-05 10:08:26 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+Rushi = 'The boy'
+print('Rushi is ', Rushi)
+
+class Rajii:
+    print('Rushi is 1 ', Rushi)
+
+    def Raj(self):
+        print('Rushi is 2 ', Rushi)
+
+obj = Rajii()
+obj.Raj()
+
+class Ram:
+    print('Rushi is 3 ', Rushi)
+
+    def Raj1(self):
+        print('Rushi is 4 ', Rushi)
+
+obj1 = Ram()
+obj1.Raj1()
